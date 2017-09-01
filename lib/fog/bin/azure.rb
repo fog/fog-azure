cb1ec1db3e32ca15bf11323d995602ee318b7631
Edward Betts <edward@4angle.com>
2017-09-01 22:19:21 +0100

    correct spelling mistake

--- lib/fog/bin/azure.rb
+++ lib/fog/bin/azure.rb
@@ -19,7 +19,7 @@
 # LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 # THE SOFTWARE.
-module Azure # deviates from other bin stuff to accomodate gem
+module Azure # deviates from other bin stuff to accommodate gem
   class << self
     def class_for(key)
       case key
